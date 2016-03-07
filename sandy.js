47b19bcf06ef9fa48ec27fbc9ec90746204325da
lyeoh <tomentosus@gmail.com>
2016-03-07 20:50:38 +0000

    Update from Alex.

--- sandy.js
+++ sandy.js
@@ -41,4 +41,57 @@ $(document).ready(function(){
 	})
 });
 
+$(document).ready(function(){
+	$('body').click(function(evt){    
+       if(evt.target.class == "navigation"){
+       	return;
+       };
+          
+       //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
+       if($(evt.target).closest('.navigation').length){
+       	return; 
+       };
+                      
+       var bar = $("#navigationBar")
+		var barWidth = bar.width() 
+		if (barWidth=0) {
+			return;
+		}
+		else {
+			bar.fadeOut();
+		};
+      
+
+});
+});
+
+$(document).ready(function(){
+$("#slideshow1 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow1 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow1');
+},  5000);
+
+});
+
+$(document).ready(function(){
+$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  5000);
+
+});
+
+
 
